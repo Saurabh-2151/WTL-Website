da347dcda1c6af8dee144311203c323a8f869fbc
Saurabh-2151 <saurabhganjale.sits.comp@gmail.com>
2025-12-11 22:51:02 +0530

    updated package

--- next.config.mjs
+++ next.config.mjs
@@ -1,34 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  images: {
-    unoptimized: true,
-  },
   experimental: {
     webpackBuildWorker: false,
   },
-  webpack: (config, { isServer }) => {
-    // Enable WebAssembly support
-    config.experiments = {
-      ...config.experiments,
-      asyncWebAssembly: true,
-      layers: true,
-    };
-    
-    // Add fallback for buffer
-    config.resolve.fallback = {
-      ...config.resolve.fallback,
-      buffer: false,
-    };
-    
+
+  webpack: (config) => {
     return config;
   },
-}
+
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+};
 
 export default nextConfig;
-
